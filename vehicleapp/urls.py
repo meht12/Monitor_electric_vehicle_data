7ca61a0e82975e2c3d9e0c6e70f285819b3f0027
meht12 <100204328+meht12@users.noreply.github.com>
2024-03-29 20:33:28 +0530

    Merge changes from main branch

--- vehicleapp/urls.py
+++ vehicleapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-   path('reports/',reports),
+   path('',reports),
    path('download-csv/', generate_csv, name='download_csv'),
    path('download-pdf/', generate_pdf, name='download_pdf'),
    path('total-miles/', total_miles_driven, name='total_miles'),
